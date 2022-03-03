0d7c554e894803dfd1981e1798f2f85d13b3a2fb
Tai Dao <taidao90@gmail.com>
2022-03-02 16:35:44 -0800

    Fix Meshcentral

--- meshcentral/startup.sh
+++ meshcentral/startup.sh
@@ -6,9 +6,7 @@ export HOSTNAME
 export CONTAINER_PORT
 export REVERSE_PROXY
 export REVERSE_PROXY_TLS_PORT
-export IFRAME
-export ALLOW_NEW_ACCOUNTS
-export WEBRTC
+
 
 if [ -f "meshcentral-data/config.json" ]
     then
@@ -17,9 +15,6 @@ if [ -f "meshcentral-data/config.json" ]
         cp config.json.template meshcentral-data/config.json
         sed -i "s_\"port\": 4430_\"port\": $CONTAINER_PORT_" meshcentral-data/config.json
         sed -i "s/\"cert\": \"myserver.mydomain.com\"/\"cert\": \"$HOSTNAME\"/" meshcentral-data/config.json
-        sed -i "s/\"NewAccounts\": true/\"NewAccounts\": \"$ALLOW_NEW_ACCOUNTS\"/" meshcentral-data/config.json
-        sed -i "s/\"WebRTC\": false/\"WebRTC\": \"$WEBRTC\"/" meshcentral-data/config.json
-        sed -i "s/\"AllowFraming\": false/\"AllowFraming\": \"$IFRAME\"/" meshcentral-data/config.json
         if [ "$REVERSE_PROXY" != "false" ]
             then 
                 sed -i "s/\"_certUrl\": \"my\.reverse\.proxy\"/\"certUrl\": \"https:\/\/$REVERSE_PROXY:$REVERSE_PROXY_TLS_PORT\"/" meshcentral-data/config.json
